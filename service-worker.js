a3b8b3a6ec35e78670ffa61860e5370ff8b5b9a9
kaushik-jr <kaushikkanthan@gmail.com>
2026-08-28 18:12:52 +0530

    Update service worker cache version and files

--- service-worker.js
+++ service-worker.js
@@ -1,63 +1,61 @@
-const CACHE_NAME = "ag-olive-attendance-v1";
+const CACHE_NAME = "ag-olive-v2";
 
 const FILES_TO_CACHE = [
-    "./",
-    "./index.html",
-    "./style.css",
-    "./app.js",
-    "./manifest.json"
+    "/",
+    "/index.html",
+    "/style.css",
+    "/app.js",
+    "/dashboard.html",
+    "/dashboard.js",
+    "/attendance.html",
+    "/attendance.js",
+    "/members.html",
+    "/members.js",
+    "/weekly.html",
+    "/weekly.js",
+    "/reports.html",
+    "/reports.js",
+    "/manifest.json"
 ];
 
 self.addEventListener("install", event => {
+    self.skipWaiting();
 
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(FILES_TO_CACHE);
-            })
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
     );
-
-    self.skipWaiting();
 });
 
-
 self.addEventListener("activate", event => {
-
     event.waitUntil(
-        caches.keys().then(cacheNames => {
-
+        caches.keys().then(keys => {
             return Promise.all(
-
-                cacheNames
-                    .filter(name => name !== CACHE_NAME)
-                    .map(name => caches.delete(name))
-
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
             );
-
         })
     );
 
     self.clients.claim();
 });
 
-
 self.addEventListener("fetch", event => {
-
     event.respondWith(
+        fetch(event.request)
+            .then(response => {
+                const responseClone = response.clone();
 
-        caches.match(event.request)
-            .then(cachedResponse => {
+                caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, responseClone);
+                });
 
-                if (cachedResponse) {
-
-                    return cachedResponse;
-
-                }
-
-                return fetch(event.request);
-
+                return response;
             })
-
+            .catch(() => {
+                return caches.match(event.request);
+            })
     );
-
 });
